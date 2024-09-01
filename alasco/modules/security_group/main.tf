cc6df1f8e13230ef9599ceadc52f0da0b2dc10a5
CharlesGM <gachangocmbugua@gmail.com>
2024-09-01 13:52:20 +0300

    trigger workflow

--- alasco/modules/security_group/main.tf
+++ alasco/modules/security_group/main.tf
@@ -9,8 +9,8 @@ resource "aws_security_group" "main" {
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
