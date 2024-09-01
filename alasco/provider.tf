8c05736228240fc66b59d474676ceb44615c77d5
CharlesGM <gachangocmbugua@gmail.com>
2024-09-01 12:22:44 +0300

    trigger workflow

--- alasco/provider.tf
+++ alasco/provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket  = "alasco-ghost"
-    key     = "terraform/state"
+    key     = "alasco-ecs-state"
     region  = "us-east-1"
     encrypt = true
   }
