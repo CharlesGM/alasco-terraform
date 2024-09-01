34415870cf151c1fd932cf7a4adfe3e8f6876742
CharlesGM <gachangocmbugua@gmail.com>
2024-09-01 12:17:26 +0300

    trigger workflow

--- s3/providers.tf
+++ s3/providers.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket  = "alasco-ghost"
-    key     = "terraform/state"
-    region  = "us-east-1"
-    encrypt = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket  = "alasco-ghost"
+#     key     = "terraform/state"
+#     region  = "us-east-1"
+#     encrypt = true
+#   }
+# }
